43cc2f6aa01c45dbdf6f623fdc8adba829841918
AirpChinpam <rdkxr678@yahoo.co.jp>
2017-06-19 17:20:40 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,32 @@
+window.alert('こんちゃ。世界。');
+
+console.log('こんちゃ。世界。');
+
+documen.getElementById('choice').textContent = new Date();
+
+var result = window.confirm('準備OK?');
+//結果を表示
+document.getElementById('choice').textContent = result;
+
+if(result){
+ document.getElementById('choice').textContent = 'OKが押されました';
+}else{
+ document.getElementById('choice').textContent = 'キャンセルが押されました';
+}
+
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください!'));
+
+var message;
+if(answer === number) {
+   message = 'あたり!';
+}else if (answer < number){
+  message = '残念!もっと大きいです';
+}else if (number < answer){
+  message = '残念!もっと小さいです';
+}else{
+ message = '0~3の数字を入力しろよ';
+}
+
+document.getElementById('choice').textContent = message;
+
